8b4a5ee0ea375eef0488c853bb37066d5c1049d0
Ryan Rembert <jrrembert@users.noreply.github.com>
2025-03-05 14:55:54 -0800

    add error handling

--- api/handler.js
+++ api/handler.js
@@ -1,8 +1,19 @@
 exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v4.0! Your function executed successfully!'
-    })
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Go Serverless v4.0! Your function executed successfully!'
+      })
+    };
+  } catch (error) {
+    console.error('Error executing function: ', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal Server Error',
+        error: error.message
+      })
+    };
+  }
 };
