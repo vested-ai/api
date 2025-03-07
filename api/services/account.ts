4a87044259beb80eb2bed727259de4ed681e77b5
Ryan Rembert <jrrembert@users.noreply.github.com>
2025-03-07 13:22:40 -0800

    stub out user registration function.

---
+++ api/services/account.ts
@@ -0,0 +1,9 @@
+export async function createAccount(email: string, password: string) {
+  return {
+    verificationCode: 'not implemented',
+  }
+}
+
+export async function sendVerificationEmail(email: string, verificationCode: string) {
+  return 'not implemented';
+}
